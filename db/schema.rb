dac6eaa14fbfa0236426d32088cd53d7e3d8ae8a
Sehong Park <sehong.park86@gamil.com>
2014-09-04 21:50:38 +0000

    user new

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140904165728) do
+ActiveRecord::Schema.define(version: 20140904212418) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140904165728) do
     t.integer  "std_num"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "phone"
   end
 
 end
