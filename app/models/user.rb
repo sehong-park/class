48f06535ae96078fabdc49ae681233f5f9f0bb5f
Sehong Park <sehong.park86@gamil.com>
2014-09-05 19:56:58 +0000

    email uniqueness validation..

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
   
+  before_save { self.email = email.downcase }
+  
   #validation
-  validates :name, presence: true, length: { minimum: 2 }
-  validates :email, presence: true
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   
 end
